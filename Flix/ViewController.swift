2b73d8a81a097bd265885ccd5fb4d92e76c13da8
Yingying Chen <yingying@Yingyings-MacBook-Air.local>
2019-02-05 16:40:08 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Yingying Chen on 2/5/19.
+//  Copyright © 2019 Yingying Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
